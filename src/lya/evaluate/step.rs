0a414dfef3c91bef95d5669267b07fa1ddcbc08f
Oleg Nizhnikov <odomontois@gmail.com>
2023-03-24 19:25:57 +0100

    - step.'static

--- src/lya/evaluate/step.rs
+++ src/lya/evaluate/step.rs
@@ -6,16 +6,16 @@ pub use super::ctx::{ContextError, EvalCtx};
 
 use super::{EvalError, EvalValue};
 
-type Cont<V, E> = dyn FnOnce(V) -> Result<Step<V, E>, E>;
+type Cont<'a, V, E> = Box<dyn FnOnce(V) -> Result<Step<'a, V, E>, E> + 'a>;
 
 #[derive(Derivative)]
 #[derivative(Default(bound = "V: Default"))]
-pub struct Step<V, E> {
+pub struct Step<'a, V, E> {
     focus: V,
-    stack: Vec<Box<Cont<V, E>>>,
+    stack: Vec<Cont<'a, V, E>>,
 }
 
-impl<V, E> From<V> for Step<V, E> {
+impl<'a, V, E> From<V> for Step<'a, V, E> {
     fn from(v: V) -> Self {
         Self {
             focus: v,
@@ -24,16 +24,16 @@ impl<V, E> From<V> for Step<V, E> {
     }
 }
 
-impl<V: 'static, E: 'static> Step<V, E> {
+impl<'a, V, E> Step<'a, V, E> {
     pub fn continue_with(
         mut self,
-        f: impl FnOnce(V) -> Result<Step<V, E>, E> + 'static,
-    ) -> Step<V, E> {
+        f: impl FnOnce(V) -> Result<Step<'a, V, E>, E> + 'a,
+    ) -> Step<'a, V, E> {
         self.stack.push(Box::new(f));
         self
     }
 
-    pub fn defer(f: impl FnOnce() -> Result<Step<V, E>, E> + 'static) -> Step<V, E>
+    pub fn defer(f: impl FnOnce() -> Result<Step<'a, V, E>, E> + 'a) -> Step<'a, V, E>
     where
         V: Default,
     {
@@ -45,7 +45,7 @@ impl<V: 'static, E: 'static> Step<V, E> {
 
     pub fn run(self) -> Result<V, E> {
         let Self { mut focus, stack } = self;
-        let mut actions: VecDeque<Box<Cont<V, E>>> = stack.into();
+        let mut actions: VecDeque<Cont<V, E>> = stack.into();
         loop {
             match actions.pop_front() {
                 Some(action) => {
@@ -61,5 +61,5 @@ impl<V: 'static, E: 'static> Step<V, E> {
     }
 }
 
-pub type EvalStep<R, E> = Step<EvalValue<R, E>, EvalError<E>>;
+pub type EvalStep<R, E> = Step<'static, EvalValue<R, E>, EvalError<E>>;
 pub type EvalStepResult<R, E> = Result<EvalStep<R, E>, EvalError<E>>;
