c416823f7c93e52b560c60f2ce18a855abc63d67
Oleg Nizhnikov <odomontois@gmail.com>
2023-03-25 12:08:54 +0100

    non-copying

--- src/lya/evaluate/ctx.rs
+++ src/lya/evaluate/ctx.rs
@@ -8,39 +8,50 @@ use super::EvalValue;
 #[derive(Derivative, Debug)]
 #[derivative(Clone(bound = ""), Default(bound = ""))]
 pub struct Context<V> {
-    vars: Rc<RefCell<Vec<Option<V>>>>,
-    arity: Rc<usize>,
+    vars: Rc<RefCell<Vec<V>>>,
+    arity: usize,
 }
 
-impl<'a, V: Clone> Context<V> {
-    pub fn pushed<'b>(&'b self, v: V) -> Self {
+impl<V: Clone> Context<V> {
+    pub fn pushed(&self, v: V) -> Self {
         let r = self.vars.borrow();
-        let mut vars = r.clone();
-        vars.push(Some(v));
-        let vars = Rc::new(RefCell::new(vars));
-        let ix = Rc::new(&*self.arity + 1);
-        Self { vars, arity: ix }
+        let vars = if self.arity == r.len() {
+            drop(r);
+            let mut r = self.vars.borrow_mut();
+            r.push(v);
+            self.vars.clone()
+        } else {
+            let mut vars: Vec<_> = r.iter().take(self.arity).cloned().collect();
+            vars.push(v);
+            Rc::new(RefCell::new(vars))
+        };
+        let arity = self.arity + 1;
+        Self { vars, arity }
     }
 
     pub fn get(&self, i: usize) -> Result<V, ContextError> {
         let r = self.vars.borrow();
-        r.get(r.len() - i - 1)
-            .cloned()
-            .flatten()
+        r.get(self.arity - i - 1)
+            .map(|x| x.clone())
             .ok_or_else(|| ContextError::NotFound)
     }
 }
 
-impl<V> Drop for Context<V> {
-    fn drop(&mut self) {
-        if let Some(&mut arity) = Rc::get_mut(&mut self.arity) {
-            if arity > 0 {
-                let mut r = self.vars.borrow_mut();
-                r[arity - 1] = None;
-            }
-        }
-    }
-}
+// impl<V> Drop for Context<V> {
+//     fn drop(&mut self) {
+//         if let Some(&mut arity) = Rc::get_mut(&mut self.arity) {
+//             if arity > 0 {
+//                 let mut r = self.vars.borrow_mut();
+//                 r[arity - 1].1 = false;
+//                 if arity == r.len() {
+//                     while r.last().map_or(false, |x| !x.1) {
+//                         r.pop();
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
 
 #[derive(Error, Debug, PartialEq, Eq)]
 pub enum ContextError {
@@ -58,5 +69,10 @@ fn lol() {
     println!(
         "{}",
         size_of::<EvalValue<u64, EvalError<ArithmeticError>>>()
+    );
+
+    println!(
+        "{}",
+        size_of::<ContextError>()
     );
 }
